074a9aabf7790fedae218a0c426413d84bc8c413
Glen1986 <escobar.glen@gmail.com>
2022-01-12 22:49:05 -0300

    primerCommit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import './index.css';
+import Counter from './Counter';
+
+const initialState = {count : 40, title:"Glen/Code"};
+function reducer(state = initialState, action){
+  return state
+}
+const store = createStore(reducer);
+
+const App = ()=> (
+  <Provider store={store}>
+    <Counter  />
+  </Provider>
+);
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
